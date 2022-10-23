72b8e6fe3e58592a807ca3044383c78920acf8cb
batelco <batel.cohen100@gmail.com>
2022-10-23 14:49:01 +0300

    delete console prints

--- client/src/my_app_api/metadataApi.js
+++ client/src/my_app_api/metadataApi.js
@@ -2,7 +2,6 @@ class MetaDataApi extends Api {
 	constructor(ingridient = "cheese", diaryFree = false, glutenFree = false, apiInterface = new AjaxCall()) {
 		let url = `http://localhost:8000/recipes/${ingridient}?diaryFree=${diaryFree}&glutenFree=${glutenFree}`
 		super(apiInterface, url)
-		console.log(ingridient, diaryFree, glutenFree)
 	}
 
 	async getData() {
