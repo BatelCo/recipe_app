c3da22632593069c741a4f42de5e2a91c6714b6d
batelco <batel.cohen100@gmail.com>
2022-10-23 12:43:58 +0300

    mvc architechture - view added

---
+++ client/src/recipeView.js
@@ -0,0 +1,19 @@
+const recipeRender = function () {
+	const renderPage = function (res) {
+		renderComponent("#recipe-template", "#results", res.metaData)
+	}
+
+	const renderComponent = function (hbTemplate, elementToRender, metaData) {
+		const source = $(hbTemplate).html()
+		const template = Handlebars.compile(source)
+		console.log(metaData)
+		let newHTML = template({ recipeData: metaData })
+		$(elementToRender).empty()
+		$(elementToRender).append(newHTML)
+	}
+
+	return {
+		renderPage,
+		renderComponent,
+	}
+}
