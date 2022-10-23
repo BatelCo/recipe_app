26ee46810b3aa7826a4ae03364827e4f65b7d6f4
batelco <batel.cohen100@gmail.com>
2022-10-23 14:29:03 +0300

    update metadata api

--- client/src/my_app_api/metadataApi.js
+++ client/src/my_app_api/metadataApi.js
@@ -1,7 +1,8 @@
 class MetaDataApi extends Api {
 	constructor(ingridient = "cheese", diaryFree = false, glutenFree = false, apiInterface = new AjaxCall()) {
-		let url = `http://localhost:8000/recipes/${ingridient}&diaryFree=${diaryFree}&glutenFree=${glutenFree}`
+		let url = `http://localhost:8000/recipes/${ingridient}?diaryFree=${diaryFree}&glutenFree=${glutenFree}`
 		super(apiInterface, url)
+		console.log(ingridient, diaryFree, glutenFree)
 	}
 
 	async getData() {
