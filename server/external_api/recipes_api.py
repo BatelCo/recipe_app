c84666ef337a602263aee0ab267f77505f6e4e99
batelco <batel.cohen100@gmail.com>
2022-10-23 14:30:19 +0300

    update recipesApi

--- server/external_api/recipes_api.py
+++ server/external_api/recipes_api.py
@@ -3,19 +3,22 @@ from .api import Api
 
 class RecipesApi(Api):
 
-    def __init__(self, ingredient):
+    def __init__(self, ingredient, diary_free, gluten_free):
         self.url = f"https://recipes-goodness.herokuapp.com/recipes/{ingredient}"
         super().__init__(self.url)
         self.raw_data = None
         self.headers = {"Content-Type": "application/json"}
+        self.diary_free = diary_free
+        self.gluten_free = gluten_free
 
 
-    def proccess_data(self, is_diary=False, is_gluten=False):
+    def proccess_data(self):
         results=[]
         results+=[{
             "title": meal["title"],
             "ingredients": meal["ingredients"],
-            "img": meal["thumbnail"]
+            "img": meal["thumbnail"],
+            "href": meal["href"]
             }
             for meal in self.raw_data["results"]]
          
