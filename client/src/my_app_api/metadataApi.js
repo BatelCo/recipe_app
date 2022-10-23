1e9add737cdf9d6a299117647f3391b63d9787c8
batelco <batel.cohen100@gmail.com>
2022-10-23 14:03:35 +0300

    url changed

--- client/src/my_app_api/metadataApi.js
+++ client/src/my_app_api/metadataApi.js
@@ -1,6 +1,6 @@
 class MetaDataApi extends Api {
 	constructor(ingridient = "cheese", diaryFree = false, glutenFree = false, apiInterface = new AjaxCall()) {
-		let url = `http://localhost:8000/recipes?ingridient=${ingridient}&diaryFree=${diaryFree}&glutenFree=${glutenFree}`
+		let url = `http://localhost:8000/recipes/${ingridient}&diaryFree=${diaryFree}&glutenFree=${glutenFree}`
 		super(apiInterface, url)
 	}
 
